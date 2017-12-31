ed832f67cd4548d63b82c6d4e405ad94ed687559
14340246 <ross.gahan-suttle@ucdconnect.ie>
2017-12-31 13:28:21 +0000

    Initial commit

---
+++ app/src/main/java/com/example/rossg_000/fyp_v3/Journal.java
@@ -0,0 +1,13 @@
+package com.example.rossg_000.fyp_v3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Journal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal);
+    }
+}
---
+++ app/src/main/java/com/example/rossg_000/fyp_v3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.rossg_000.fyp_v3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.graphics.Color;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the button */
+    public void goToPage(View view) {
+        Intent intent = new Intent(this, Tasks.class);
+        startActivity(intent);
+    }
+
+    public void goToJournal(View view) {
+        Intent intent = new Intent(this, Journal.class);
+        startActivity(intent);
+    }
+}
