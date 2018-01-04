f006d7db0eaf7d7e56b0c51c4e963bfd9f58b4c7
14340246 <ross.gahan-suttle@ucdconnect.ie>
2018-01-04 18:24:31 +0000

    Display hashmap (manually input) containing activity and number (amount required for completion)

--- app/src/main/java/com/example/rossg_000/fyp_v3/Tasks.java
+++ app/src/main/java/com/example/rossg_000/fyp_v3/Tasks.java
@@ -1,58 +1,74 @@
 package com.example.rossg_000.fyp_v3;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.app.Activity;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
-import java.util.Random;
 
-public class Tasks extends AppCompatActivity {
-
+public class Tasks extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+        ListView taskListView = (ListView) findViewById(R.id.taskListView);
+        final HashMap<String, Integer> testHash = getHashMapTest();
+        List<HashMap<String, String>> listItems = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_items, new String[]{"First Line", "Second Line"}, new int[]{R.id.text1, R.id.text2});
+        Iterator iterator = testHash.entrySet().iterator();
 
-
-        Integer max = 10;
-        String[] taskArray = new String[10];
-        Random rand = new Random();
-
-        //Simple random number generator, replace with
-        // random (no duplicate) tasks later, have random
-        //selection only occur for empty places in array
-        for(Integer i=0;i<taskArray.length;i++) {
-            Integer next = rand.nextInt(max) + 1;
-            String temp = Integer.toString(next);
-            taskArray[i] = temp;
+        while(iterator.hasNext())
+        {
+            HashMap<String, String> newMap = new HashMap<>();
+            Map.Entry pairs = (Map.Entry)iterator.next();
+            newMap.put("First Line", pairs.getKey().toString());
+            newMap.put("Second Line", pairs.getValue().toString());
+            listItems.add(newMap);
         }
 
-
-
-
-        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, taskArray);
-
-        ListView taskListView = (ListView) findViewById(R.id.taskListView);
         taskListView.setAdapter(adapter);
 
-        taskListView.setOnItemClickListener(
-            new AdapterView.OnItemClickListener(){
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    String elementClicked = String.valueOf(adapterView.getItemAtPosition(i));
-                    //when item in list is clicked, do stuff to it here
-                    Toast.makeText(Tasks.this, elementClicked, Toast.LENGTH_LONG).show(); //prints out at bottom of screen for a few seconds
-
+        taskListView.setOnItemClickListener
+        (
+                new AdapterView.OnItemClickListener()
+                {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+                    {
+                        String elementClicked = String.valueOf(adapterView.getItemAtPosition(i));
+                        //when item in list is clicked, do stuff to it here
+                        Toast.makeText(Tasks.this, elementClicked, Toast.LENGTH_LONG).show(); //prints out at bottom of screen for a few seconds
+                    }
                 }
-            }
         );
     }
+
+    public HashMap<String, Integer> getHashMapTest()
+    {
+        final HashMap<String, Integer> testHash = new HashMap<>();
+        testHash.put("Walk (miles):", 8);
+        testHash.put("Jog (miles):", 5);
+        testHash.put("Swim (Lengths):", 10);
+        testHash.put("Push Ups:", 50);
+        testHash.put("Meditation (minutes):", 20);
+        testHash.put("Deep Breathes:", 4);
+        testHash.put("Hike (minutes):", 40);
+        return testHash;
+
+    }
+
+
 
 
 }
