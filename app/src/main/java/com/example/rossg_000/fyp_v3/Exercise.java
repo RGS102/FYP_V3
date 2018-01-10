ecd675d2e2be5c4229f807a09b728d4f9d968aea
14340246 <ross.gahan-suttle@ucdconnect.ie>
2018-01-10 15:11:01 +0000

    updated it so that tapping one of the tasks will increase a certain value (currently level) and holding it will decrease a certain value (currently level). Need to implement a "database" method that checks the ID of the task and the task level and then returns the other values corresponding to that level of that task

--- app/src/main/java/com/example/rossg_000/fyp_v3/Exercise.java
+++ app/src/main/java/com/example/rossg_000/fyp_v3/Exercise.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -32,18 +33,7 @@ public class Exercise extends AppCompatActivity {
         setContentView(R.layout.activity_exercise);
 
         taskDetails = (ListView) findViewById(R.id.taskListView);
-        /*
-        taskDetailsListTest = new ArrayList<>();
-
-        taskDetailsListTest.add(new TaskDetails(1, "Walk", 100, "steps", "Level:", 1,1,1, 1, 1));
-        taskDetailsListTest.add(new TaskDetails(2, "Run", 1, "mile(s)", "Level:", 1,2,2,2,2));
-        taskDetailsListTest.add(new TaskDetails(3, "Jog", 2, "mile(s)", "Level:", 1,3,3,3,3));
-        taskDetailsListTest.add(new TaskDetails(4, "Swim", 20, "lengths", "Level:", 2,4,4,4,4));
-        taskDetailsListTest.add(new TaskDetails(5, "Cycle", 10, "mile(s)", "Level:", 5, 5,5,5,5));
-        */
-
         loadData();
-
         adapter = new TaskDetailsAdapter(getApplicationContext(), taskDetailsListTest);
         taskDetails.setAdapter(adapter);
 
@@ -62,35 +52,51 @@ public class Exercise extends AppCompatActivity {
                 int cMinutes = clickedList.getMinutes();
                 int cSeconds = clickedList.getSeconds();
 
-                //Toast.makeText(getApplicationContext(), "Clicked = " + cCountDownTimer, Toast.LENGTH_LONG).show();
 
                 if (cHours == 0 && cMinutes == 0 && cSeconds == 0) {cSeconds = 60; cMinutes = 59; cHours = 23; cDays -= 1;}
                 else if (cMinutes == 0 && cSeconds == 0) {cSeconds = 60; cMinutes = 59; cHours -= 1;}
                 else if (cSeconds == 0) {cSeconds = 60; cMinutes -= 1;}
 
-
-                //taskDetailsListTest.set(i, new TaskDetails(cId, cTaskName, cRequirmentInteger, cRequirmentString, cLevelString, cLevelInteger, cDays, cHours, cMinutes, cSeconds - 1));
-
-
-                 if(cRequirmentInteger==0)
-                 {
-                    taskDetailsListTest.set(i, new TaskDetails(cId, cTaskName, cRequirmentInteger+5, cRequirmentString, cLevelString, cLevelInteger+1, cDays, cHours, cMinutes, cSeconds-1));
-                 }
-                 else
-                 {
-                    taskDetailsListTest.set(i, new TaskDetails(cId, cTaskName, cRequirmentInteger - 1, cRequirmentString, cLevelString, cLevelInteger, cDays, cHours, cMinutes, cSeconds-1));
-                 }
-
+                taskDetailsListTest.set(i, new TaskDetails(cId, cTaskName, cRequirmentInteger, cRequirmentString, cLevelString, cLevelInteger+1, cDays, cHours, cMinutes, cSeconds));
 
                 adapter = new TaskDetailsAdapter(getApplicationContext(), taskDetailsListTest);
                 taskDetails.setAdapter(adapter);
                 saveData();
 
             }
+
+
         });
 
+
+        taskDetails.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TaskDetails clickedList = taskDetailsListTest.get(i);
+                int cId = clickedList.getId();
+                String cTaskName = clickedList.getTaskName();
+                int cRequirmentInteger = clickedList.getTaskRequirementInteger();
+                String cRequirmentString = clickedList.getTaskRequirementString();
+                String cLevelString = clickedList.getTaskLevelString();
+                int cLevelInteger = clickedList.getTaskLevelInteger();
+                int cDays = clickedList.getDays();
+                int cHours = clickedList.getHours();
+                int cMinutes = clickedList.getMinutes();
+                int cSeconds = clickedList.getSeconds();
+
+                taskDetailsListTest.set(i, new TaskDetails(cId, cTaskName, cRequirmentInteger, cRequirmentString, cLevelString, cLevelInteger-1, cDays, cHours, cMinutes, cSeconds));
+
+                adapter = new TaskDetailsAdapter(getApplicationContext(), taskDetailsListTest);
+                taskDetails.setAdapter(adapter);
+                saveData();
+
+                return true;
+            }
+        });
         saveData();
     }
+
+
 
     private void saveData(){
         SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
